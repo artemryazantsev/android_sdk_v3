3e5f170f45f5b98a6c36e133a5e501e086fb160c
Usmaan Dad <Usmaan.Dad@bjss.com>
2016-10-16 01:24:23 +0100

    Reduce the amount of constructors

--- Demo/library/src/main/java/android/mobileconnect/gsma/com/library/view/InteractiveWebView.java
+++ Demo/library/src/main/java/android/mobileconnect/gsma/com/library/view/InteractiveWebView.java
@@ -2,7 +2,6 @@ package android.mobileconnect.gsma.com.library.view;
 
 import android.content.Context;
 import android.os.Build;
-import android.util.AttributeSet;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -17,22 +16,10 @@ public class InteractiveWebView extends WebView
     public InteractiveWebView(final Context context)
     {
         super(context);
-        initialise(context);
+        initialise();
     }
 
-    public InteractiveWebView(final Context context, final AttributeSet attrs)
-    {
-        super(context, attrs);
-        initialise(context);
-    }
-
-    public InteractiveWebView(final Context context, final AttributeSet attrs, final int defStyleAttr)
-    {
-        super(context, attrs, defStyleAttr);
-        initialise(context);
-    }
-
-    private void initialise(final Context context)
+    private void initialise()
     {
         if (!isInEditMode())
         {
@@ -50,7 +37,7 @@ public class InteractiveWebView extends WebView
             setFocusableInTouchMode(true);
             requestFocus(View.FOCUS_DOWN);
 
-            WebSettings settings = getSettings();
+            final WebSettings settings = getSettings();
 
             settings.setJavaScriptEnabled(true);
             settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
@@ -59,9 +46,6 @@ public class InteractiveWebView extends WebView
             settings.setDatabaseEnabled(true);
             settings.setSupportZoom(false);
             settings.setUseWideViewPort(false);
-
-            String databasePath = context.getApplicationContext().getDir("database", Context.MODE_PRIVATE).getPath();
-            settings.setDatabasePath(databasePath);
         }
     }
 }
